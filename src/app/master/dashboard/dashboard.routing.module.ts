0dd2455cdd528f32bc2108c09e55f6bb33ebd6b2
arjunkumar4396 <arjun4396kumar@yahoo.com>
2019-11-18 23:40:07 +0530

    added routing and navigation in header

--- src/app/master/dashboard/dashboard.routing.module.ts
+++ src/app/master/dashboard/dashboard.routing.module.ts
@@ -7,6 +7,10 @@ import { ContactUsComponent } from './contacts/contact-us.component';
 import { FaqComponent } from './contacts/faq/faq.component';
 import { ImpactComponent } from './impact/impact.component';
 import { OurModelsComponent } from './our-models/our-models.component';
+import { OurImpactComponent } from './impact/our-impact/our-impact.component';
+import { MenstruationChallengesComponent } from './our-models/menstruation-challenges/menstruation-challenges.component';
+import { OurInitiativesComponent } from './our-models/our-initiatives/our-initiatives.component';
+import { SanitaryPadsComponent } from './our-models/sanitary-pads/sanitary-pads.component';
 
 
 const routes: Routes = [
@@ -27,7 +31,19 @@ const routes: Routes = [
   },
   {
     path:'our-models',component:OurModelsComponent
-  }
+  },
+  {
+    path:'our-models/mensturation-challenges',component:MenstruationChallengesComponent
+  },
+  {
+    path:'our-models/our-initiatives',component:OurInitiativesComponent
+  },
+  {
+    path:'our-models/sanitary-pads',component:SanitaryPadsComponent
+  },
+  {
+    path:'impact/our-impact',component:OurImpactComponent
+  },
 ];
 
 @NgModule({
